3067d6b14058ba290f7fb6e5edc0f611a7f1a779
unknown <ullhaq@hotmail.com>
2020-06-08 23:06:45 +0200

    cypher command execution

--- src/core/neo4j_core.py
+++ src/core/neo4j_core.py
@@ -1,14 +1,19 @@
 import py2neo as neo
 
-graph = neo.Graph("bolt://localhost:7687", auth=("neo4j", "12345"))
+graph = neo.Graph("bolt://neo4j:7687", auth=("neo4j", "12345"))
+
 
 def sync_data():
-    tx = graph.begin()
-    tx.run("""
-    LOAD CSV WITH HEADERS FROM "https://github.com/HazemSaeid/SSI-exam-project/raw/master/src/resources/covid.csv" AS row
-    MERGE (s:State {name: row.state})
-    MERGE (c:Cases {date: row.date, amount: row.cases, deaths:row.deaths})
-    MERGE (co:County {name: row.county})
-    MERGE (s)-[h:HAS]->(c)
-    MERGE (c)-[r:REGISTERED]->(co)
-    """)
+    try:
+        graph.run("""
+        LOAD CSV WITH HEADERS FROM "https://github.com/HazemSaeid/SSI-exam-project/raw/master/src/resources/sample-covid.csv" AS row
+        MERGE (s:State {name: row.state})
+        MERGE (c:Cases {date: row.date, amount: row.cases, deaths:row.deaths})
+        MERGE (co:County {name: row.county})
+        MERGE (s)-[h:HAS]->(c)
+        MERGE (c)-[r:REGISTERED]->(co)
+        """)
+
+        return 'neo4j data up to date'
+    except:
+        return 'An exception occurred'
