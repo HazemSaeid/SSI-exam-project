34209bb093aacfcae69804e4fee2abc0ce3fd6d8
unknown <ullhaq@hotmail.com>
2020-06-08 21:57:21 +0200

    neo4j data insertion & postgres

--- src/app.py
+++ src/app.py
@@ -1,14 +1,76 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import core.mongodb_core as cm
+import core.neo4j_core as cn
 from routes.cases import case_routes
 from routes.deaths import death_routes
+import psycopg2
 app = Flask(__name__)
-
+app.debug = True
 
 app.register_blueprint(case_routes)
 app.register_blueprint(death_routes)
 
+# def token_required(f):
+#    @wraps(f)
+#    def decorator(*args, **kwargs):
+#       token = None
 
-@app.route("/")
+#       if 'x-access-tokens' in request.headers:
+#          token = request.headers['x-access-tokens']
+
+#       if not token:
+#          return jsonify({'message': 'a valid token is missing'})
+
+#       try:
+#          data = jwt.decode(token, app.config[SECRET_KEY])
+#          current_user = Users.query.filter_by(public_id=data['public_id']).first()
+#       except:
+#          return jsonify({'message': 'token is invalid'})
+
+#         return f(current_user, *args, **kwargs)
+#    return decorator
+
+# @app.route('/login', methods='POST')
+# def authenticate():
+#   auth = request.authorization
+
+#   if not auth or not auth.username or not auth.password:
+#      return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+#   conn = psycopg2.connect('host=postgres dbname=covid user=postgres password=12345678')
+#   cur = conn.cursor()
+#   cur.execute('SELECT * FROM users WHERE name=%s', [username])
+#   user = cur.fetchone()
+#   conn.close()
+
+#   if check_password_hash(user.password, auth.password):
+#      token = jwt.encode({'sub': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, 'oausdnoaisdioasdu')
+#      return jsonify({'token' : token.decode('UTF-8')})
+
+#   return make_response('could not verify',  401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+
+# @app.route('/setup')
+# def setup():
+#     try:
+#         conn = psycopg2.connect('host=postgres dbname=covid user=postgres password=12345678')
+#         cur = conn.cursor()
+#         cur.execute('DROP TABLE IF EXISTS users; CREATE TABLE users (id int PRIMARY KEY, name varchar, password varchar);')
+#         cur.execute("INSERT INTO users (id, name) VALUES (1, 'admin', '), (2, 'manager');")
+#         conn.commit()
+#         cur.close()
+#         conn.close()
+
+#         return 'Setup successful'
+#     except:
+#         return 'An exception occurred'
+
+
+@app.route('/refresh')
+def sync_data():
+    return cn.sync_data()
+
+
+@app.route('/')
 def hello_world():
     return cm.add_initial_covid_data()
--- src/core/neo4j_core.py
+++ src/core/neo4j_core.py
@@ -1,39 +1,14 @@
 import py2neo as neo
-import asyncio
-from objects.objects import CovidCase
 
-graph = neo.Graph("bolt://localhost:7687", auth=("neo4j","12345"))
+graph = neo.Graph("bolt://localhost:7687", auth=("neo4j", "12345"))
 
-#async def insert_csv_data():
-    #stmt = """ 
-    #LOAD CSV WITH HEADERS FROM src\resources\us-counties.csv AS row 
-    #MERGE (s:State {state: row.name})
-    #MERGE (c:Cases {date: row.date, amount: row.amount, deaths:row.deaths})
-    #MERGE (co:County {name: row.county})
-    #"""
-
-
-async def add_covid_data(_state, _county,_date, _cases, _deaths):
+def sync_data():
     tx = graph.begin()
-    state = neo.Node("State", name = _state )
-    county = neo.Node("County", name = _county)
-    cases = neo.Node(_date, amount = _cases, deaths = _deaths)
-
-    state_rel_county = neo.Relationship(state, "HAS", county)
-    county_rel_cases = neo.Relationship(county, "REGISTERED", cases)
-
-    tx.merge(state, primary_label="State", primary_key=("name"))
-    tx.merge(county,primary_label="County", primary_key=("name"))
-    tx.merge(state_rel_county, primary_label="HAS", primary_key=("state","county"))
-    tx.create(cases)
-    tx.create(county_rel_cases)
-    tx.commit()
-
-async def get_cases_from_county(_county):
-    tx = graph.begin()
-    stmt = graph.run(f"MATCH (c:County)-[r:REGISTERED]-(ca:Cases) where c.name = '{_county}' RETURN  ca limit 5000").data()
-
-
-
-asyncio.run(add_covid_data("Texas","Orange County","2020-05-14",12,22))
-#asyncio.run(get_cases_from_county("Orange County"))
+    tx.run("""
+    LOAD CSV WITH HEADERS FROM "https://github.com/HazemSaeid/SSI-exam-project/raw/master/src/resources/covid.csv" AS row
+    MERGE (s:State {name: row.state})
+    MERGE (c:Cases {date: row.date, amount: row.cases, deaths:row.deaths})
+    MERGE (co:County {name: row.county})
+    MERGE (s)-[h:HAS]->(c)
+    MERGE (c)-[r:REGISTERED]->(co)
+    """)
