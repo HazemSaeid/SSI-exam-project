52414d41242d95e7d4b8ec2c68f703f062416a9a
unknown <ullhaq@hotmail.com>
2020-06-09 11:25:21 +0200

    neo4j data import

--- src/core/neo4j_core.py
+++ src/core/neo4j_core.py
@@ -6,7 +6,8 @@ graph = neo.Graph("bolt://neo4j:7687", auth=("neo4j", "12345"))
 def sync_data():
     try:
         graph.run("""
-        LOAD CSV WITH HEADERS FROM "https://github.com/HazemSaeid/SSI-exam-project/raw/master/src/resources/sample-covid.csv" AS row
+        USING PERIODIC COMMIT 1000
+        LOAD CSV WITH HEADERS FROM "file:///covid.csv" AS row
         MERGE (s:State {name: row.state})
         MERGE (c:Cases {date: row.date, amount: row.cases, deaths:row.deaths})
         MERGE (co:County {name: row.county})
